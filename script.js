c54e44b651a1d4087c0aae2c3516ce8be0af4543
India Byrne <byrneindia@gmail.com>
2020-03-02 14:47:30 -0700

    first push unsure of js.

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ function initShoppingList() {
 	let form = document.getElementById("item-form");
 
 	form.addEventListener("submit", (event) => {
-		handleItemForm (event, form);
+		handleItemForm(event, form);
+	});
 }
 function handleItemForm(event, formRef) {
 	if(event.preventDefault()) {
@@ -14,26 +15,35 @@ function handleItemForm(event, formRef) {
 	addItemToShoppingList();
 
 	return false;
-
-	let itemHtml =addItemToShoppingList();
-	let itemListRef = document.getElementById("shopping list");
-		itemListRef.insertAdjacentElement("afterend,")
-
-	function addItemToShoppingList() {
-		let itemName = document.getElementById("item");
-		let itemAmount = document.getElementById("item-amount");
-		let id = getRandomInt(0, 10000000)
-	}
-function createListItemHtml() {
-		return '<li id= "item${id}">
-	 ${itemName} - ${itemAmount}
-	<button type="button">Delete Item</button>
-	</li>';
-	}
-	function setDeleteButtonEvent(id):
-	letdeletButton = document.getElementById("button"+id)
-	deletButton.addEventListener("click", ()) =>
 }
+	function addItemToShoppingList() {
+		let itemName = document.getElementById("item-name");
+		let itemAmount = document.getElementById("item-amount");
+		let id = getRandomInt(0, 10000000);
+
+		//Creates list item html and appends to page.
+		let itemHtml =createListItemHtml(itemName.value, itemAmount.value);
+		console.log("Item HTML:",);
+		let itemListRef = document.getElementById("shopping list");
+		itemListRef.insertAdjacentElement("afterend", "itemHTML")
+
+		setDeleteButtonEvent(id);
+}
+function setDeleteButtonEvent(id):
+	letdeleteButton= document.getElementById("button"+id);
+deleteButton.addEventListener("click", ()) =>
+});
+	function creatListItemHtml (itemName, itemAmount) {
+		return '<li id="item${id}'>
+			${itemName} - ${itemName}
+			<button id="buttons{id}" type ="button">Delete</button>
+			< /li>';
+	}
+	function removeListItem(id) {
+		let listItem= document.getElementById("item" + id);
+		listitem.parentNode.removeChild(listItem);
+	}
+
 function  getRandomInt(min, max) {
 		min=Math.ceil(min);
 		max =Math.floor(max);
